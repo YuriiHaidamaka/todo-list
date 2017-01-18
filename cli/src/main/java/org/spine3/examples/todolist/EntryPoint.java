919f4a54069ac8ccac9797478fa0ae62b5304f28
illiashepilov <illia.shepilov@teamdev.com>
2017-01-18 11:47:46 +0200

    Separated the command mode and the application entry point

--- cli/src/main/java/org/spine3/examples/todolist/EntryPoint.java
+++ cli/src/main/java/org/spine3/examples/todolist/EntryPoint.java
@@ -20,24 +20,11 @@
 
 package org.spine3.examples.todolist;
 
-import asg.cliche.Command;
-import asg.cliche.Shell;
-import asg.cliche.ShellDependent;
 import asg.cliche.ShellFactory;
 import com.google.common.base.Charsets;
-import org.spine3.examples.todolist.c.commands.AssignLabelToTask;
-import org.spine3.examples.todolist.c.commands.CompleteTask;
-import org.spine3.examples.todolist.c.commands.DeleteTask;
-import org.spine3.examples.todolist.c.commands.RemoveLabelFromTask;
-import org.spine3.examples.todolist.c.commands.ReopenTask;
-import org.spine3.examples.todolist.c.commands.RestoreDeletedTask;
 import org.spine3.examples.todolist.client.CommandLineTodoClient;
 import org.spine3.examples.todolist.client.TodoClient;
-import org.spine3.examples.todolist.modes.CreateLabelMode;
-import org.spine3.examples.todolist.modes.CreateTaskMode;
-import org.spine3.examples.todolist.modes.ObtainViewMode;
-import org.spine3.examples.todolist.modes.UpdateLabelMode;
-import org.spine3.examples.todolist.modes.UpdateTaskMode;
+import org.spine3.examples.todolist.modes.MainMode;
 import org.spine3.examples.todolist.server.Server;
 import org.spine3.server.storage.memory.InMemoryStorageFactory;
 import org.spine3.util.Exceptions;
@@ -49,167 +36,35 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.spine3.client.ConnectionConstants.DEFAULT_CLIENT_SERVICE_PORT;
+import static org.spine3.examples.todolist.modes.MainMode.HELP_ADVICE;
 
 /**
  * @author Illia Shepilov
  */
-public class EntryPoint implements ShellDependent {
+public class EntryPoint {
 
-    private static final String HELP_ADVICE = "Enter 'help' to view all commands";
-    private Shell shell;
-    private TodoClient client = new CommandLineTodoClient("localhost", DEFAULT_CLIENT_SERVICE_PORT);
-    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, Charsets.UTF_8));
+    private static final String TODO_PROMPT = "todo";
 
-    private static final String CHOOSE_THE_COMMAND = "Choose the command:\n" +
-            "1:  Create task mode\n" +
-            "2:  Update task mode\n" +
-            "3:  Create label mode\n" +
-            "4:  Update label mode\n" +
-            "5:  Assign label to task\n" +
-            "6:  Remove label from task\n" +
-            "7:  Delete task\n" +
-            "8:  Reopen task\n" +
-            "9:  Restore task\n" +
-            "10: Complete task\n" +
-            "11: Obtain views";
-
-    @Override
-    public void cliSetShell(Shell theShell) {
-        this.shell = theShell;
-    }
-
-    @Command(abbrev = "0")
-    public String help() throws IOException {
-        return CHOOSE_THE_COMMAND;
-    }
-
-    @Command(abbrev = "1")
-    public void createTask() throws IOException {
-        ShellFactory.createSubshell("create-task", shell, "Create task mode\n" + HELP_ADVICE, new CreateTaskMode(client, reader))
-                    .commandLoop();
-    }
-
-    @Command(abbrev = "2")
-    public void updateTask() throws IOException {
-        ShellFactory.createSubshell("update-task", shell, "Update task mode\n" + HELP_ADVICE, new UpdateTaskMode(client, reader))
-                    .commandLoop();
-    }
-
-    @Command(abbrev = "3")
-    public void createLabel() throws IOException {
-        ShellFactory.createSubshell("create-label", shell, "Create label mode\n" + HELP_ADVICE, new CreateLabelMode(client, reader))
-                    .commandLoop();
-    }
-
-    @Command(abbrev = "4")
-    public void updateLabel() throws IOException {
-        ShellFactory.createSubshell("update-label", shell, "Update label mode\n" + HELP_ADVICE, new UpdateLabelMode(client, reader))
-                    .commandLoop();
-    }
-
-    @Command(abbrev = "5")
-    public void assignLabel() throws IOException {
-        System.out.println("Please enter the task id: ");
-        final TaskId taskId = TaskId.newBuilder()
-                                    .setValue(reader.readLine())
-                                    .build();
-        System.out.println("Please enter the label id: ");
-        final TaskLabelId labelId = TaskLabelId.newBuilder()
-                                               .setValue(reader.readLine())
-                                               .build();
-        final AssignLabelToTask assignLabelToTask = AssignLabelToTask.newBuilder()
-                                                                     .setId(taskId)
-                                                                     .setLabelId(labelId)
-                                                                     .build();
-        client.assignLabel(assignLabelToTask);
-    }
-
-    @Command(abbrev = "6")
-    public void removeLabel() throws IOException {
-        System.out.println("Please enter the task id: ");
-        final TaskId taskId = TaskId.newBuilder()
-                                    .setValue(reader.readLine())
-                                    .build();
-        System.out.println("Please enter the label id: ");
-        final TaskLabelId labelId = TaskLabelId.newBuilder()
-                                               .setValue(reader.readLine())
-                                               .build();
-        final RemoveLabelFromTask removeLabelFromTask = RemoveLabelFromTask.newBuilder()
-                                                                           .setId(taskId)
-                                                                           .setLabelId(labelId)
-                                                                           .build();
-        client.removeLabel(removeLabelFromTask);
-    }
-
-    @Command(abbrev = "7")
-    public void deleteTask() throws IOException {
-        System.out.println("Please enter the task id: ");
-        final TaskId taskId = TaskId.newBuilder()
-                                    .setValue(reader.readLine())
-                                    .build();
-        final DeleteTask deleteTask = DeleteTask.newBuilder()
-                                                .setId(taskId)
-                                                .build();
-        client.delete(deleteTask);
-    }
-
-    @Command(abbrev = "8")
-    public void reopenTask() throws IOException {
-        System.out.println("Please enter the task id: ");
-        final TaskId taskId = TaskId.newBuilder()
-                                    .setValue(reader.readLine())
-                                    .build();
-        final ReopenTask reopenTask = ReopenTask.newBuilder()
-                                                .setId(taskId)
-                                                .build();
-        client.reopen(reopenTask);
-    }
-
-    @Command(abbrev = "9")
-    public void restoreTask() throws IOException {
-        System.out.println("Please enter the task id: ");
-        final TaskId taskId = TaskId.newBuilder()
-                                    .setValue(reader.readLine())
-                                    .build();
-        final RestoreDeletedTask restoreDeletedTask = RestoreDeletedTask.newBuilder()
-                                                                        .setId(taskId)
-                                                                        .build();
-        client.restore(restoreDeletedTask);
-    }
-
-    @Command(abbrev = "10")
-    public void completeTask() throws IOException {
-        System.out.println("Please enter the task id: ");
-        final TaskId taskId = TaskId.newBuilder()
-                                    .setValue(reader.readLine())
-                                    .build();
-        final CompleteTask completeTask = CompleteTask.newBuilder()
-                                                      .setId(taskId)
-                                                      .build();
-        client.complete(completeTask);
-    }
-
-    @Command(abbrev = "11")
-    public void obtainViews() throws IOException {
-        ShellFactory.createSubshell("obtain-views", shell, "Obtain view mode\n" + HELP_ADVICE, new ObtainViewMode(client, reader))
-                    .commandLoop();
-    }
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        startServer();
-        ShellFactory.createConsoleShell("todo", HELP_ADVICE, new EntryPoint())
-                    .commandLoop();
-    }
-
-    private static void startServer() throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         final InMemoryStorageFactory storageFactory = InMemoryStorageFactory.getInstance();
         final Server server = new Server(storageFactory);
+        startServer(server);
 
+        final TodoClient client = new CommandLineTodoClient("localhost", DEFAULT_CLIENT_SERVICE_PORT);
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, Charsets.UTF_8));
+        final MainMode entryPoint = new MainMode(client, reader);
+        ShellFactory.createConsoleShell(TODO_PROMPT, HELP_ADVICE, entryPoint)
+                    .commandLoop();
+        reader.close();
+        client.shutdown();
+        server.shutdown();
+    }
+
+    private static void startServer(Server server) throws InterruptedException {
         final CountDownLatch serverStartLatch = new CountDownLatch(1);
         final Thread serverThread = new Thread(() -> {
             try {
                 server.start();
-                server.awaitTermination();
                 serverStartLatch.countDown();
             } catch (IOException e) {
                 throw Exceptions.wrappedCause(e);
@@ -217,6 +72,6 @@ public class EntryPoint implements ShellDependent {
         });
 
         serverThread.start();
-        serverStartLatch.await(100, TimeUnit.MILLISECONDS);
+        serverStartLatch.await(1500, TimeUnit.MILLISECONDS);
     }
 }
---
+++ cli/src/main/java/org/spine3/examples/todolist/modes/MainMode.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2017, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.spine3.examples.todolist.modes;
+
+import asg.cliche.Command;
+import asg.cliche.Shell;
+import asg.cliche.ShellDependent;
+import asg.cliche.ShellFactory;
+import org.spine3.examples.todolist.TaskId;
+import org.spine3.examples.todolist.TaskLabelId;
+import org.spine3.examples.todolist.c.commands.AssignLabelToTask;
+import org.spine3.examples.todolist.c.commands.CompleteTask;
+import org.spine3.examples.todolist.c.commands.DeleteTask;
+import org.spine3.examples.todolist.c.commands.RemoveLabelFromTask;
+import org.spine3.examples.todolist.c.commands.ReopenTask;
+import org.spine3.examples.todolist.c.commands.RestoreDeletedTask;
+import org.spine3.examples.todolist.client.TodoClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import static org.spine3.examples.todolist.modes.ModeHelper.sendMessageToUser;
+
+/**
+ * @author Illia Shepilov
+ */
+public class MainMode implements ShellDependent {
+
+    public static final String HELP_ADVICE = "Enter 'help' to view all commands.";
+    private static final String CREATE_TASK_PROMPT = "create-task";
+    private static final String UPDATE_TASK_PROMPT = "update-task";
+    private static final String CREATE_LABEL_PROMPT = "create-label";
+    private static final String UPDATE_LABEL_PROMPT = "update-label";
+    private static final String OBTAIN_VIEWS_PROMPT = "obtain-views";
+    private static final String ENTER_TASK_ID_MESSAGE = "Please enter the task id: ";
+    private static final String ENTER_LABEL_ID_MESSAGE = "Please enter the label id: ";
+    private static final String CREATE_TASK_MODE = "******************Create task mode******************\n";
+    private static final String CREATE_TASK_TITLE = CREATE_TASK_MODE + HELP_ADVICE;
+    private static final String UPDATE_TASK_MODE = "******************Update task mode******************\n";
+    private static final String UPDATE_TASK_TITLE = UPDATE_TASK_MODE + HELP_ADVICE;
+    private static final String CREATE_LABEL_MODE = "******************Create label mode*****************\n";
+    private static final String CREATE_LABEL_TITLE = CREATE_LABEL_MODE + HELP_ADVICE;
+    private static final String UPDATE_LABEL_MODE = "******************Update label mode*****************\n";
+    private static final String UPDATE_LABEL_TITLE = UPDATE_LABEL_MODE + HELP_ADVICE;
+    private static final String OBTAIN_VIEW_MODE = "******************Obtain view mode******************\n";
+    private static final String OBTAIN_VIEW_TITLE = OBTAIN_VIEW_MODE + HELP_ADVICE;
+    private static final String HELP_COMMAND = "0:    Help.\n" +
+            "1:    Create the task mode.\n" +
+            "2:    Update the task mode.\n" +
+            "3:    Create the label mode.\n" +
+            "4:    Update the label mode.\n" +
+            "5:    Assign label to task mode.\n" +
+            "6:    Remove label from task mode.\n" +
+            "7:    Delete task mode.\n" +
+            "8:    Reopen task mode.\n" +
+            "9:    Restore task mode.\n" +
+            "10:   Complete task mode.\n" +
+            "11:   Obtain views mode.\n" +
+            "exit: Exit from the mode.";
+    private final TodoClient client;
+    private final BufferedReader reader;
+    private Shell shell;
+
+    public MainMode(TodoClient client, BufferedReader reader) {
+        this.client = client;
+        this.reader = reader;
+    }
+
+    @Override
+    public void cliSetShell(Shell theShell) {
+        this.shell = theShell;
+    }
+
+    @Command(abbrev = "0")
+    public String help() throws IOException {
+        return HELP_COMMAND;
+    }
+
+    @Command(abbrev = "1")
+    public void createTask() throws IOException {
+        ShellFactory.createSubshell(CREATE_TASK_PROMPT, shell, CREATE_TASK_TITLE, new CreateTaskMode(client, reader))
+                    .commandLoop();
+    }
+
+    @Command(abbrev = "2")
+    public void updateTask() throws IOException {
+        ShellFactory.createSubshell(UPDATE_TASK_PROMPT, shell, UPDATE_TASK_TITLE, new UpdateTaskMode(client, reader))
+                    .commandLoop();
+    }
+
+    @Command(abbrev = "3")
+    public void createLabel() throws IOException {
+        ShellFactory.createSubshell(CREATE_LABEL_PROMPT, shell, CREATE_LABEL_TITLE, new CreateLabelMode(client, reader))
+                    .commandLoop();
+    }
+
+    @Command(abbrev = "4")
+    public void updateLabel() throws IOException {
+        ShellFactory.createSubshell(UPDATE_LABEL_PROMPT, shell, UPDATE_LABEL_TITLE, new UpdateLabelMode(client, reader))
+                    .commandLoop();
+    }
+
+    @Command(abbrev = "5")
+    public void assignLabel() throws IOException {
+        sendMessageToUser(ENTER_TASK_ID_MESSAGE);
+        final TaskId taskId = TaskId.newBuilder()
+                                    .setValue(reader.readLine())
+                                    .build();
+        sendMessageToUser(ENTER_LABEL_ID_MESSAGE);
+        final TaskLabelId labelId = TaskLabelId.newBuilder()
+                                               .setValue(reader.readLine())
+                                               .build();
+        final AssignLabelToTask assignLabelToTask = AssignLabelToTask.newBuilder()
+                                                                     .setId(taskId)
+                                                                     .setLabelId(labelId)
+                                                                     .build();
+        client.assignLabel(assignLabelToTask);
+    }
+
+    @Command(abbrev = "6")
+    public void removeLabel() throws IOException {
+        sendMessageToUser(ENTER_TASK_ID_MESSAGE);
+        final TaskId taskId = TaskId.newBuilder()
+                                    .setValue(reader.readLine())
+                                    .build();
+        sendMessageToUser(ENTER_LABEL_ID_MESSAGE);
+        final TaskLabelId labelId = TaskLabelId.newBuilder()
+                                               .setValue(reader.readLine())
+                                               .build();
+        final RemoveLabelFromTask removeLabelFromTask = RemoveLabelFromTask.newBuilder()
+                                                                           .setId(taskId)
+                                                                           .setLabelId(labelId)
+                                                                           .build();
+        client.removeLabel(removeLabelFromTask);
+    }
+
+    @Command(abbrev = "7")
+    public void deleteTask() throws IOException {
+        sendMessageToUser(ENTER_TASK_ID_MESSAGE);
+        final TaskId taskId = TaskId.newBuilder()
+                                    .setValue(reader.readLine())
+                                    .build();
+        final DeleteTask deleteTask = DeleteTask.newBuilder()
+                                                .setId(taskId)
+                                                .build();
+        client.delete(deleteTask);
+    }
+
+    @Command(abbrev = "8")
+    public void reopenTask() throws IOException {
+        sendMessageToUser(ENTER_TASK_ID_MESSAGE);
+        final TaskId taskId = TaskId.newBuilder()
+                                    .setValue(reader.readLine())
+                                    .build();
+        final ReopenTask reopenTask = ReopenTask.newBuilder()
+                                                .setId(taskId)
+                                                .build();
+        client.reopen(reopenTask);
+    }
+
+    @Command(abbrev = "9")
+    public void restoreTask() throws IOException {
+        sendMessageToUser(ENTER_TASK_ID_MESSAGE);
+        final TaskId taskId = TaskId.newBuilder()
+                                    .setValue(reader.readLine())
+                                    .build();
+        final RestoreDeletedTask restoreDeletedTask = RestoreDeletedTask.newBuilder()
+                                                                        .setId(taskId)
+                                                                        .build();
+        client.restore(restoreDeletedTask);
+    }
+
+    @Command(abbrev = "10")
+    public void completeTask() throws IOException {
+        sendMessageToUser(ENTER_TASK_ID_MESSAGE);
+        final TaskId taskId = TaskId.newBuilder()
+                                    .setValue(reader.readLine())
+                                    .build();
+        final CompleteTask completeTask = CompleteTask.newBuilder()
+                                                      .setId(taskId)
+                                                      .build();
+        client.complete(completeTask);
+    }
+
+    @Command(abbrev = "11")
+    public void obtainViews() throws IOException {
+        ShellFactory.createSubshell(OBTAIN_VIEWS_PROMPT, shell, OBTAIN_VIEW_TITLE, new ObtainViewMode(client))
+                    .commandLoop();
+    }
+}
