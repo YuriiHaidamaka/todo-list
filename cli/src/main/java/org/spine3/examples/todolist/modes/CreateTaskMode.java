1fb9bb7026d329ed495879d35680cdc75c1a4b1d
illiashepilov <illia.shepilov@teamdev.com>
2017-01-23 13:44:58 +0200

    Rework the CreateTaskMode class

--- cli/src/main/java/org/spine3/examples/todolist/modes/CreateTaskMode.java
+++ cli/src/main/java/org/spine3/examples/todolist/modes/CreateTaskMode.java
@@ -21,6 +21,7 @@
 package org.spine3.examples.todolist.modes;
 
 import asg.cliche.Command;
+import com.google.common.collect.Maps;
 import com.google.protobuf.Timestamp;
 import com.google.protobuf.util.Timestamps;
 import jline.console.ConsoleReader;
@@ -36,52 +37,67 @@ import org.spine3.examples.todolist.c.commands.UpdateTaskDescription;
 import org.spine3.examples.todolist.c.commands.UpdateTaskDueDate;
 import org.spine3.examples.todolist.c.commands.UpdateTaskPriority;
 import org.spine3.examples.todolist.client.TodoClient;
+import org.spine3.examples.todolist.client.builder.TaskBuilder;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Map;
 
 import static org.spine3.base.Identifiers.newUuid;
 import static org.spine3.examples.todolist.DateHelper.getDateFormat;
-import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.CHANGED_DESCRIPTION_MESSAGE;
-import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.CHANGED_DUE_DATE_MESSAGE;
-import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.CHANGED_PRIORITY_MESSAGE;
 import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.CREATED_DRAFT_MESSAGE;
 import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.CREATED_TASK_MESSAGE;
+import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.CREATE_TASK_PROMPT;
+import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.CREATE_TASK_TITLE;
 import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.DRAFT_FINALIZED_MESSAGE;
 import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.EMPTY;
 import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.HELP_MESSAGE;
 import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.NEED_TO_FINALIZE_MESSAGE;
-import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.POSITIVE_ANSWER;
 import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.SET_DESCRIPTION_MESSAGE;
 import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.SET_DUE_DATE_MESSAGE;
 import static org.spine3.examples.todolist.modes.CreateTaskMode.CreateTaskModeConstants.SET_PRIORITY_MESSAGE;
+import static org.spine3.examples.todolist.modes.GeneralMode.MainModeConstants.HELP_ADVICE;
 import static org.spine3.examples.todolist.modes.ModeHelper.constructUserFriendlyDate;
 import static org.spine3.examples.todolist.modes.ModeHelper.sendMessageToUser;
 
 /**
  * @author Illia Shepilov
  */
-@SuppressWarnings("unused")
 public class CreateTaskMode extends Mode {
 
+    private static final String SET_PRIORITY_APPROVE_MESSAGE = "Do you want to set the task priority?(y/n)";
+    private static final String SET_DUE_DATE_APPROVE_MESSAGE = "Do you want to set the task due date?(y/n)";
+    private static final String CREATE_TASK_APPROVE_MESSAGE = "Do you want to create one more task?(y/n)";
+    private static final String NEGATIVE_ANSWER = "n";
     private Timestamp dueDate = Timestamp.getDefaultInstance();
     private TaskPriority priority = TaskPriority.TP_UNDEFINED;
     private String description;
+    private Map<String, Mode> map = Maps.newHashMap();
 
     CreateTaskMode(TodoClient client, ConsoleReader reader) {
         super(client, reader);
+        map.put("0", new HelpMode(client, reader, HELP_MESSAGE));
+        map.put("1", new CreateDraftDM(client, reader));
+        map.put("2", new CreateTaskFM(client, reader));
     }
 
     @Override
     void start() throws IOException {
-        String line;
-        while ((line = reader.readLine())!=null){
-            if(line.equals("back")){
+        sendMessageToUser(CREATE_TASK_TITLE);
+        reader.setPrompt(CREATE_TASK_PROMPT);
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            if (line.equals("back")) {
+                reader.setPrompt("todo>");
                 return;
             }
-
+            final Mode mode = map.get(line);
+            if (mode != null) {
+                mode.start();
+                reader.setPrompt("todo>");
+            }
+            sendMessageToUser("Incorrect command.");
         }
     }
 
@@ -90,75 +106,55 @@ public class CreateTaskMode extends Mode {
         sendMessageToUser(HELP_MESSAGE);
     }
 
-    @Command(abbrev = "1")
-    public void setTaskDescription() throws IOException {
-        final String description = obtainDescriptionValue(SET_DESCRIPTION_MESSAGE, true);
-        this.description = description;
-        final String message = CHANGED_DESCRIPTION_MESSAGE + description;
-        sendMessageToUser(message);
-    }
-
-    @Command(abbrev = "2")
-    public void setTaskDueDate() throws IOException, ParseException {
-        final String dueDateValue = obtainDueDateValue(SET_DUE_DATE_MESSAGE, true);
-        final SimpleDateFormat simpleDateFormat = getDateFormat();
-        final long dueDateInMS = simpleDateFormat.parse(dueDateValue)
-                                                 .getTime();
-        final Timestamp dueDate = Timestamps.fromMillis(dueDateInMS);
-        this.dueDate = dueDate;
-        final String message = CHANGED_DUE_DATE_MESSAGE + constructUserFriendlyDate(dueDateInMS);
-        sendMessageToUser(message);
-    }
-
-    @Command(abbrev = "3")
-    public void setTaskPriority() throws IOException {
-        final String priority = obtainPriorityValue(SET_PRIORITY_MESSAGE);
-        this.priority = TaskPriority.valueOf(priority.toUpperCase());
-        final String message = CHANGED_PRIORITY_MESSAGE + priority;
-        sendMessageToUser(message);
-    }
-
-    @Command(abbrev = "4")
-    public void createTaskDraft() throws IOException {
+    private void createTaskDraft() throws IOException {
         final TaskId taskId = TaskId.newBuilder()
                                     .setValue(newUuid())
                                     .build();
         createTaskDraft(taskId);
-        updatePriorityIfNeeded(taskId);
-        updateDueDateIfNeeded(taskId);
+        updateTaskValuesIfNeeded(taskId);
+
         final String userFriendlyDate = constructUserFriendlyDate(Timestamps.toMillis(dueDate));
         final String idValue = taskId.getValue();
         final String result = String.format(CREATED_DRAFT_MESSAGE, idValue, description, priority, userFriendlyDate);
         sendMessageToUser(result);
+
         finalizeDraftIfNeeded(taskId);
         clearValues();
     }
 
-    @Command(abbrev = "5")
-    public void createTask() throws IOException {
+    private void createTask() throws IOException {
         final TaskId taskId = TaskId.newBuilder()
                                     .setValue(newUuid())
                                     .build();
         createTask(taskId);
-        updatePriorityIfNeeded(taskId);
-        updateDueDateIfNeeded(taskId);
+        updateTaskValuesIfNeeded(taskId);
+
         final String userFriendlyDate = constructUserFriendlyDate(Timestamps.toMillis(dueDate));
         final String idValue = taskId.getValue();
         final String result = String.format(CREATED_TASK_MESSAGE, idValue, description, priority, userFriendlyDate);
         sendMessageToUser(result);
+
         clearValues();
     }
 
-    private void createTaskDraft(TaskId taskId) throws IOException {
-        final boolean isValid = descriptionValidator.validate(description);
-        if (!isValid) {
-            setTaskDescription();
+    private void updateTaskValuesIfNeeded(TaskId taskId) throws IOException {
+        updatePriorityIfNeeded(taskId);
+        //TODO
+        try {
+            updateDueDateIfNeeded(taskId);
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
+    }
+
+    private void createTaskDraft(TaskId taskId) throws IOException {
+        final String description = obtainDescriptionValue(SET_DESCRIPTION_MESSAGE, true);
 
         final CreateDraft createTask = CreateDraft.newBuilder()
                                                   .setId(taskId)
                                                   .build();
         client.create(createTask);
+
         final StringChange change = StringChange.newBuilder()
                                                 .setPreviousValue(EMPTY)
                                                 .setNewValue(description)
@@ -168,58 +164,73 @@ public class CreateTaskMode extends Mode {
                                                                                  .setDescriptionChange(change)
                                                                                  .build();
         client.update(updateTaskDescription);
+        this.description = description;
     }
 
     private void createTask(TaskId taskId) throws IOException {
-        final boolean isValid = descriptionValidator.validate(description);
-        if (!isValid) {
-            setTaskDescription();
-        }
+        final String description = obtainDescriptionValue(SET_DESCRIPTION_MESSAGE, true);
 
         final CreateBasicTask createTask = CreateBasicTask.newBuilder()
                                                           .setId(taskId)
                                                           .setDescription(description)
                                                           .build();
         client.create(createTask);
+        this.description = description;
     }
 
     private void finalizeDraftIfNeeded(TaskId taskId) throws IOException {
-        sendMessageToUser(NEED_TO_FINALIZE_MESSAGE);
-        final String input = reader.readLine();
-        final boolean isValid = approveValidator.validate(input);
-        if (input.equals(POSITIVE_ANSWER)) {
-            final FinalizeDraft finalizeDraft = FinalizeDraft.newBuilder()
-                                                             .setId(taskId)
-                                                             .build();
-            client.finalize(finalizeDraft);
-            sendMessageToUser(DRAFT_FINALIZED_MESSAGE);
+        final String approveValue = obtainApproveValue(NEED_TO_FINALIZE_MESSAGE);
+        if (approveValue.equals(NEGATIVE_ANSWER)) {
+            return;
         }
+        final FinalizeDraft finalizeDraft = FinalizeDraft.newBuilder()
+                                                         .setId(taskId)
+                                                         .build();
+        client.finalize(finalizeDraft);
+        sendMessageToUser(DRAFT_FINALIZED_MESSAGE);
     }
 
-    private void updateDueDateIfNeeded(TaskId taskId) {
-        if (dueDate != Timestamp.getDefaultInstance()) {
-            final TimestampChange change = TimestampChange.newBuilder()
-                                                          .setNewValue(dueDate)
-                                                          .build();
-            final UpdateTaskDueDate updateTaskDueDate = UpdateTaskDueDate.newBuilder()
-                                                                         .setId(taskId)
-                                                                         .setDueDateChange(change)
-                                                                         .build();
-            client.update(updateTaskDueDate);
+    private void updateDueDateIfNeeded(TaskId taskId) throws IOException, ParseException {
+        final String approveValue = obtainApproveValue(SET_DUE_DATE_APPROVE_MESSAGE);
+        if (approveValue.equals(NEGATIVE_ANSWER)) {
+            return;
         }
+
+        final String dueDateValue = obtainDueDateValue(SET_DUE_DATE_MESSAGE, true);
+        final SimpleDateFormat simpleDateFormat = getDateFormat();
+        final long newDueDateInMS = simpleDateFormat.parse(dueDateValue)
+                                                    .getTime();
+        final Timestamp dueDate = Timestamps.fromMillis(newDueDateInMS);
+
+        final TimestampChange change = TimestampChange.newBuilder()
+                                                      .setNewValue(dueDate)
+                                                      .build();
+        final UpdateTaskDueDate updateTaskDueDate = UpdateTaskDueDate.newBuilder()
+                                                                     .setId(taskId)
+                                                                     .setDueDateChange(change)
+                                                                     .build();
+        client.update(updateTaskDueDate);
+        this.dueDate = dueDate;
     }
 
-    private void updatePriorityIfNeeded(TaskId taskId) {
-        if (priority != TaskPriority.TP_UNDEFINED) {
-            final PriorityChange change = PriorityChange.newBuilder()
-                                                        .setNewValue(priority)
-                                                        .build();
-            final UpdateTaskPriority updateTaskPriority = UpdateTaskPriority.newBuilder()
-                                                                            .setId(taskId)
-                                                                            .setPriorityChange(change)
-                                                                            .build();
-            client.update(updateTaskPriority);
+    private void updatePriorityIfNeeded(TaskId taskId) throws IOException {
+        final String approveValue = obtainApproveValue(SET_PRIORITY_APPROVE_MESSAGE);
+        if (approveValue.equals(NEGATIVE_ANSWER)) {
+            return;
         }
+
+        final String priorityValue = obtainPriorityValue(SET_PRIORITY_MESSAGE);
+        final TaskPriority priority = TaskPriority.valueOf(priorityValue);
+
+        final PriorityChange change = PriorityChange.newBuilder()
+                                                    .setNewValue(priority)
+                                                    .build();
+        final UpdateTaskPriority updateTaskPriority = UpdateTaskPriority.newBuilder()
+                                                                        .setId(taskId)
+                                                                        .setPriorityChange(change)
+                                                                        .build();
+        client.update(updateTaskPriority);
+        this.priority = priority;
     }
 
     private void clearValues() {
@@ -228,7 +239,61 @@ public class CreateTaskMode extends Mode {
         this.dueDate = Timestamp.getDefaultInstance();
     }
 
+    class CreateTaskFM extends Mode {
+
+        CreateTaskFM(TodoClient client, ConsoleReader reader) {
+            super(client, reader);
+        }
+
+        @Override
+        void start() throws IOException {
+            sendMessageToUser(CREATE_TASK_TITLE);
+            reader.setPrompt(CREATE_TASK_PROMPT);
+            String line = "";
+            while (line != null) {
+                if (line.equals("back")) {
+                    return;
+                }
+                createTask();
+                final String approveValue = obtainApproveValue(CREATE_TASK_APPROVE_MESSAGE);
+                if (approveValue.equals(NEGATIVE_ANSWER)) {
+                    return;
+                }
+                line = reader.readLine();
+            }
+        }
+    }
+
+    class CreateDraftDM extends Mode {
+
+        CreateDraftDM(TodoClient client, ConsoleReader reader) {
+            super(client, reader);
+        }
+
+        @Override
+        void start() throws IOException {
+            sendMessageToUser(CREATE_TASK_TITLE);
+            reader.setPrompt(CREATE_TASK_PROMPT);
+            String line = "";
+            while (line != null) {
+                if (line.equals("back")) {
+                    return;
+                }
+                createTaskDraft();
+                final String approveValue = obtainApproveValue(CREATE_TASK_APPROVE_MESSAGE);
+                if (approveValue.equals(NEGATIVE_ANSWER)) {
+                    return;
+                }
+                line = reader.readLine();
+            }
+        }
+    }
+
     static class CreateTaskModeConstants {
+        static final String CREATE_TASK_PROMPT = "create-task>";
+        static final String CREATE_TASK_MODE = "******************** Create task menu ********************\n";
+        static final String CREATE_TASK_TITLE = CREATE_TASK_MODE + HELP_ADVICE +
+                CreateTaskMode.CreateTaskModeConstants.HELP_MESSAGE;
         static final String EMPTY = "";
         static final String NEED_TO_FINALIZE_MESSAGE = "Do you want to finalize the created task draft?(y/n)";
         static final String DRAFT_FINALIZED_MESSAGE = "Task draft finalized.";
@@ -240,12 +305,9 @@ public class CreateTaskMode extends Mode {
         static final String SET_DUE_DATE_MESSAGE = "Please enter the task due date: ";
         static final String SET_PRIORITY_MESSAGE = "Please enter the task priority";
         static final String HELP_MESSAGE = "0:    Help.\n" +
-                "1:    Set the task description.\n" +
-                "2:    Set the task due date.\n" +
-                "3:    Set the task priority.\n" +
-                "4:    Create the task with specified parameters[description is required].\n" +
-                "5:    Create the task with specified parameters[description is required][FAST MODE].\n" +
-                "exit: Exit from the mode.";
+                "1:    Create the task with specified parameters[description is required].\n" +
+                "2:    Create the task with specified parameters[description is required][FAST MODE].\n" +
+                "back: Back to the previous menu.";
         static final String CREATED_DRAFT_MESSAGE = "Created task draft with parameters:" +
                 "\nid: %s\ndescription: %s\npriority: %s\ndue date: %s";
         static final String CREATED_TASK_MESSAGE = "Created task with parameters:" +
