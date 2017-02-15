e0843e57f3d7bd6f1e1c640a3b46651a10d45ddf
illiashepilov <illia.shepilov@teamdev.com>
2017-02-15 18:19:34 +0200

    Rename `ModeHelper` to `TodoListCommands`.

--- cli/src/main/java/org/spine3/examples/todolist/mode/TodoListCommands.java
+++ cli/src/main/java/org/spine3/examples/todolist/mode/TodoListCommands.java
@@ -21,7 +21,6 @@
 package org.spine3.examples.todolist.mode;
 
 import com.google.protobuf.Timestamp;
-import com.google.protobuf.util.Timestamps;
 import org.spine3.change.StringChange;
 import org.spine3.change.TimestampChange;
 import org.spine3.examples.todolist.LabelColor;
@@ -37,39 +36,17 @@ import org.spine3.examples.todolist.c.commands.UpdateLabelDetails;
 import org.spine3.examples.todolist.c.commands.UpdateTaskDescription;
 import org.spine3.examples.todolist.c.commands.UpdateTaskDueDate;
 import org.spine3.examples.todolist.c.commands.UpdateTaskPriority;
-import org.spine3.examples.todolist.q.projections.DraftTasksView;
-import org.spine3.examples.todolist.q.projections.LabelledTasksView;
-import org.spine3.examples.todolist.q.projections.MyListView;
-import org.spine3.examples.todolist.q.projections.TaskView;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-
-import static org.spine3.examples.todolist.DateHelper.getDateFormat;
-import static org.spine3.examples.todolist.mode.CommonMode.CommonModeConstants.DEFAULT_VALUE;
 import static org.spine3.examples.todolist.mode.CreateTaskMode.CreateTaskModeConstants.EMPTY;
-import static org.spine3.examples.todolist.mode.Mode.ModeConstants.LINE_SEPARATOR;
 
 /**
+ * Serves as utility class which provides methods for commands constructing.
+ *
  * @author Illia Shepilov
  */
-//TODO:2017-02-15:illiashepilov: fix warnings
-class ModeHelper {
+class TodoListCommands {
 
-    private static final String MY_LIST_TASKS = "My list tasks";
-    private static final String DRAFT_TASKS = "Draft tasks";
-    private static final String LABELLED_TASKS = "Labelled tasks";
-    private static final String TASK = "Task: ";
-    private static final String LABEL_ID_VALUE = "Label id: ";
-    private static final String TASK_ID_VALUE = "Task id: ";
-    private static final String LABEL_TITLE_VALUE = "Label title: ";
-    private static final String LABEL_COLOR_VALUE = "Label color: ";
-    private static final String DESCRIPTION_VALUE = "Description: ";
-    private static final String PRIORITY_VALUE = "Priority: ";
-    private static final String DUE_DATE_VALUE = "Due date: ";
-
-    private ModeHelper() {
+    private TodoListCommands() {
     }
 
     static LabelDetailsChange createLabelDetailsChange(LabelDetails newLabelDetails,
@@ -188,85 +165,5 @@ class ModeHelper {
                                                     .setNewValue(newTaskPriority)
                                                     .build();
         return result;
-    }
-
-    static String constructUserFriendlyDate(long millis) {
-        final SimpleDateFormat simpleDateFormat = getDateFormat();
-        final String date = millis == 0 ? DEFAULT_VALUE : simpleDateFormat.format(new Date(millis));
-        return date;
-    }
-
-    static String constructUserFriendlyMyList(MyListView myListView) {
-        final StringBuilder builder = new StringBuilder();
-        final List<TaskView> viewList = myListView.getMyList()
-                                                  .getItemsList();
-        builder.append(MY_LIST_TASKS);
-        builder.append(LINE_SEPARATOR);
-        for (TaskView view : viewList) {
-            constructUserFriendlyTaskView(builder, view);
-        }
-        return builder.toString();
-    }
-
-    static String constructUserFriendlyDraftTasks(DraftTasksView draftTasksView) {
-        final StringBuilder builder = new StringBuilder();
-        final List<TaskView> viewList = draftTasksView.getDraftTasks()
-                                                      .getItemsList();
-        builder.append(DRAFT_TASKS);
-        builder.append(LINE_SEPARATOR);
-        for (TaskView view : viewList) {
-            constructUserFriendlyTaskView(builder, view);
-        }
-        return builder.toString();
-    }
-
-    static String constructUserFriendlyLabelledTasks(List<LabelledTasksView> labelledTasksView) {
-        final StringBuilder builder = new StringBuilder();
-        builder.append(LABELLED_TASKS);
-        builder.append(LINE_SEPARATOR);
-        for (LabelledTasksView labelledView : labelledTasksView) {
-            constructLabelledView(builder, labelledView);
-        }
-
-        return builder.toString();
-    }
-
-    private static void constructLabelledView(StringBuilder builder, LabelledTasksView labelledView) {
-        builder.append(LABEL_ID_VALUE);
-        builder.append(LINE_SEPARATOR);
-        builder.append(LABEL_TITLE_VALUE);
-        builder.append(labelledView.getLabelTitle());
-        builder.append(LINE_SEPARATOR);
-        builder.append(LABEL_COLOR_VALUE);
-        builder.append(labelledView.getLabelColor());
-        builder.append(LINE_SEPARATOR);
-        final List<TaskView> viewList = labelledView.getLabelledTasks()
-                                                    .getItemsList();
-        for (TaskView view : viewList) {
-            constructUserFriendlyTaskView(builder, view);
-        }
-    }
-
-    private static void constructUserFriendlyTaskView(StringBuilder builder, TaskView view) {
-        builder.append(TASK);
-        builder.append(LINE_SEPARATOR);
-        builder.append(TASK_ID_VALUE);
-        final String taskIdValue = view.getId()
-                                       .getValue();
-        builder.append(taskIdValue);
-        builder.append(LINE_SEPARATOR);
-        builder.append(DESCRIPTION_VALUE);
-        builder.append(view.getDescription());
-        builder.append(LINE_SEPARATOR);
-        builder.append(PRIORITY_VALUE);
-        builder.append(view.getPriority());
-        builder.append(LINE_SEPARATOR);
-        builder.append(DUE_DATE_VALUE);
-        final String date = constructUserFriendlyDate(Timestamps.toMillis(view.getDueDate()));
-        builder.append(date);
-        builder.append(LINE_SEPARATOR);
-        builder.append(LABEL_ID_VALUE);
-        builder.append(view.getLabelId());
-        builder.append(LINE_SEPARATOR);
     }
 }
